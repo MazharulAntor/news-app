0e7ac5a2732520acf501ae308178475e8b6d4b4c
Md. Mazharul Islam Antor <antormazharul@gmail.com>
2022-12-26 09:18:09 +0600

    Cards

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
       <div>
-        This is a news item.
+        <div className="card" style={{width: '18rem'}}>
+            <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
       </div>
     )
   }
