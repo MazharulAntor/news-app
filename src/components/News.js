0dd69cf76eba218535c032f1b59b26435a8c3401
MazharulAntor <mazharulantor2015@gmail.com>
2023-01-16 10:40:12 +0600

    Fetch API to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -47,11 +47,18 @@ export class News extends Component {
   ]
   constructor(){
     super();
-    console.log("I am a constructor from News component");
     this.state={
         articles: this.articles,
         loading: false
     }
+  }
+
+  async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=ef2527fe6d3f4b069c1dcbc689085d71";
+    let data= await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles})
   }
 
   render() {
@@ -61,7 +68,7 @@ export class News extends Component {
         <div className='row'>
         {this.state.articles.map((element)=>{
           return <div className='col-md-4' key={element.url}>
-          <NewsItem title={element.title} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          <NewsItem title={element.title} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
         </div>
         })}           
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,10 +8,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: '18rem'}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://cdn.dnaindia.com/sites/default/files/styles/half/public/2023/01/15/2566707-saturn-icy-moon.jpg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description} ...</p>
+                    <p className="card-text">{description}...</p>
                     <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
                 </div>
             </div>
